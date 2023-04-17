aa65087a1a4e2abdae9e8f44751c794a504f9f0a
USER <USER@10.0.104.190>
2023-04-17 12:28:53 +0800

    init

---
+++ BradJava/src/tw/org/iii/tutor/Brad01.java
@@ -0,0 +1,27 @@
+package tw.org.iii.tutor;
+
+public class Brad01 {
+	public static void main(String[] args) {
+		// byte 2^8 => -128 ~ 127, 
+		// short 2^16 => 65536 => -32768 ~ 32767, 
+		// int 2^32, long 2^64
+		byte var1;
+		byte var2, var3;
+		byte var4 = 123;
+		
+		System.out.println(var4);
+		
+		int var5 = var4 + 1;
+		short var6 = 321, var7 = 123;
+		int var8 = var6 + var7;
+		//---------------------------------
+		// [a-zA-Z$_][a-zA-Z0-9$_]*
+		byte $_$ = 101;
+		System.out.println($_$);
+		int 變數1 = 123;
+		System.out.println(變數1);
+		
+		
+	}
+
+}
