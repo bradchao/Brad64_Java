a1179b24ea5d051739038ffc29e2d3b168a139b5
USER <USER@10.0.104.190>
2023-04-17 12:38:33 +0800

    04171238

--- BradJava/src/tw/org/iii/tutor/HelloWorld.java
+++ BradJava/src/tw/org/iii/tutor/HelloWorld.java
@@ -2,6 +2,6 @@ package tw.org.iii.tutor;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World2");
+		System.out.println("Hello, World4");
 	}
 }
