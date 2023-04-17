36490cbcdbcd12320c5b69d2462e8fbb7bab6d51
USER <USER@10.0.104.190>
2023-04-17 14:40:17 +0800

    04171420

---
+++ BradJava/src/tw/org/iii/tutor/Brad05.java
@@ -0,0 +1,18 @@
+package tw.org.iii.tutor;
+
+public class Brad05 {
+
+	public static void main(String[] args) {
+		char c1 = 'a';	// 97
+		char c2 = 65;	// 2^16 => 0 ~ 65535
+		System.out.println(c1);
+		System.out.println(c2);
+		int var1 = c1 + c2;
+		System.out.println(var1);
+		char c3 = '資';
+		int var2 = c3;
+		System.out.println(var2);
+		
+	}
+
+}
