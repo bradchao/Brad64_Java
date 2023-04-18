a724c0e3b97a8c7529bab2918046984e1136ac30
USER <USER@10.0.104.190>
2023-04-18 11:34:41 +0800

    04181134

--- BradJava/src/tw/org/iii/tutor/Brad16.java
+++ BradJava/src/tw/org/iii/tutor/Brad16.java
@@ -3,32 +3,31 @@ package tw.org.iii.tutor;
 public class Brad16 {
 
 	public static void main(String[] args) {
-		int p0, p1, p2, p3, p4, p5, p6;
-		p0 = p1 = p2 = p3 = p4 = p5 = p6 = 0;
+		int[] p = new int[7];
 		
-		for (int i=0; i<100; i++) {
-			int point = (int)(Math.random()*6+1);	// 1 ~ 6
-			switch(point) {
-				case 1: p1++; break;
-				case 2: p2++; break;
-				case 3: p3++; break;
-				case 4: p4++; break;
-				case 5: p5++; break;
-				case 6: p6++; break;
-				default: p0++;
+		for (int i=0; i<10000000; i++) {
+			int point = (int)(Math.random()*9+1);	// 1 ~ 6
+			if (point >= 1 && point <= 9) {
+				p[point>=7?point-3:point]++;
+			}else {
+				p[0]++;
 			}
 		}
 		
-		if (p0 == 0) {
-			System.out.printf("1點出現%d次\n", p1);
-			System.out.printf("2點出現%d次\n", p2);
-			System.out.printf("3點出現%d次\n", p3);
-			System.out.printf("4點出現%d次\n", p4);
-			System.out.printf("5點出現%d次\n", p5);
-			System.out.printf("6點出現%d次\n", p6);
+		if (p[0] == 0) {
+			for (int i=1; i<=6; i++) {
+				System.out.printf("%d點出現%d次\n", i, p[i]);
+			}
 		}else {
-			System.out.println("debug = " + p0);
+			System.out.println("debug = " + p[0]);
 		}
+		System.out.println("----");
+		
+		// for-each
+		for (int v : p) {
+			System.out.println(v);
+		}
+		
 		
 		
 	}
