6b1b8adfba4ee57950ee676a6a3a48c48b231443
USER <USER@10.0.104.190>
2023-04-18 15:44:23 +0800

    04181544

--- BradJava/src/tw/org/iii/tutor/PokerV2.java
+++ BradJava/src/tw/org/iii/tutor/PokerV2.java
@@ -3,9 +3,21 @@ package tw.org.iii.tutor;
 public class PokerV2 {
 
 	public static void main(String[] args) {
-		long startTime = System.currentTimeMillis();
+		int[] poker = shuffle();
+		// 發牌
+		
+		int[][] players = new int[4][13];
+		for (int i=0; i<poker.length; i++) {
+			players[i%4][i/4] = poker[i];
+		}
+		
+		
+		
+	}
+	
+	static int[] shuffle() {
 		// 1. 洗牌
-		int count = 10;
+		int count = 52;
 		int[] poker = new int[count];
 		boolean isRepeat;
 		int temp;
@@ -25,15 +37,13 @@ public class PokerV2 {
 			}while (isRepeat);
 			
 			poker[i] = temp;
+			System.out.println(poker[i]);
 		}
-		
-		for (int v: poker) {
-			System.out.println(v);
-		}
-		
 		System.out.println("---");
-		System.out.println(System.currentTimeMillis()-startTime);
-		
+		return poker;
 	}
+	
+	
+	
 
 }
