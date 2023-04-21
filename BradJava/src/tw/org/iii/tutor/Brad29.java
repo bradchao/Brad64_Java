7e3043c8dce95a94b0f63a6c270baac7d16cb2db
USER <USER@10.0.104.190>
2023-04-21 10:40:19 +0800

    04211040

--- BradJava/src/tw/org/iii/tutor/Brad29.java
+++ BradJava/src/tw/org/iii/tutor/Brad29.java
@@ -4,7 +4,7 @@ public class Brad29 {
 	public static void main(String[] args) {
 		Brad292 obj1 = new Brad292();
 		Brad291 obj2 = new Brad292();
-		Brad291 obj3 = new Brad293();
+		Brad293 obj3 = new Brad293();
 		doThing(obj3);
 	}
 	
@@ -18,6 +18,6 @@ class Brad292 extends Brad291 {
 	void m2() {System.out.println("Brad292:m2()");}
 }
 class Brad293 extends Brad291 {
-	void m1() {System.out.println("Brad293:m1()");}
+	//void m1() {System.out.println("Brad293:m1()");}
 	void m2() {System.out.println("Brad293:m2()");}
 }
---
+++ BradJava/src/tw/org/iii/tutor/GNum.java
@@ -0,0 +1,44 @@
+package tw.org.iii.tutor;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class GNum extends JFrame {
+	private JButton guess;
+	private JTextField input;
+	private JTextArea log;
+	
+	public GNum() {
+		super("猜數字遊戲");
+		
+		guess = new JButton("猜");
+		input = new JTextField();
+		log = new JTextArea();
+		
+		setLayout(new BorderLayout());
+		JPanel top = new JPanel(new BorderLayout());
+		top.add(guess, BorderLayout.EAST);
+		top.add(input, BorderLayout.CENTER);
+		
+		add(top, BorderLayout.NORTH);
+		add(log, BorderLayout.CENTER);
+		
+		
+		
+		
+		
+		
+		setSize(640, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args) {
+		new GNum();
+	}
+
+}
