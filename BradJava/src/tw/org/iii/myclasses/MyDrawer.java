63f858ac108ebcfd8c90604a09e170a40e87dc85
USER <USER@10.0.104.190>
2023-04-24 16:31:01 +0800

    04241630

--- BradJava/src/tw/org/iii/myclasses/MyDrawer.java
+++ BradJava/src/tw/org/iii/myclasses/MyDrawer.java
@@ -1,11 +1,27 @@
 package tw.org.iii.myclasses;
 
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
 public class MyDrawer extends JPanel {
 	public MyDrawer() {
 		setBackground(Color.YELLOW);
+		MyListener myListener = new MyListener();
+		addMouseListener(myListener);
+		addMouseMotionListener(myListener);
+	}
+	
+	private class MyListener extends MouseAdapter{
+		@Override
+		public void mousePressed(MouseEvent e) {
+			System.out.println("press:" + e.getX() + ", " + e.getY());
+		}
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			System.out.println("drag:" + e.getX() + ", " + e.getY());
+		}
 	}
 }
