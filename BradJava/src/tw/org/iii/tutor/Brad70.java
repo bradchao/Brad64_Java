e7dba5f5b9bc9caab9536f195e54b094b6aa41d3
USER <USER@10.0.104.190>
2023-05-02 11:32:16 +0800

    05021132

--- BradJava/src/tw/org/iii/tutor/Brad70.java
+++ BradJava/src/tw/org/iii/tutor/Brad70.java
@@ -9,6 +9,9 @@ import java.net.URLConnection;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Brad70 {
 	public static void main(String[] args) {
 		try {
@@ -20,16 +23,29 @@ public class Brad70 {
 			BufferedReader reader = 
 				new BufferedReader(
 					new InputStreamReader(conn.getInputStream()));
-			String line;
+			String line; StringBuffer sb = new StringBuffer();
 			while ( (line = reader.readLine()) != null) {
-				System.out.println(line);
+				sb.append(line);
 			}
 			reader.close();
-			
 			System.out.println("OK");
+			
+			parseJSON(sb.toString());
+			
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 		
 	}
+	
+	// parse JSON
+	static void parseJSON(String json) {
+		JSONArray root = new JSONArray(json);
+		for (int i=0; i<root.length(); i++) {
+			JSONObject row = root.getJSONObject(i);
+			System.out.println(row.getString("Name"));
+		}
+	}
+	
+	
 }
