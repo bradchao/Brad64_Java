6bf47864012ddc38e6410413d9c3f14b17b55810
USER <USER@10.0.104.190>
2023-05-03 12:23:27 +0800

    333

---
+++ BradJava/src/tw/org/iii/tutor/BallGame.java
@@ -0,0 +1,28 @@
+package tw.org.iii.tutor;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import tw.org.iii.myclasses.MyPanel;
+
+public class BallGame extends JFrame {
+	private MyPanel myPanel;
+	
+	public BallGame() {
+		super("Ball Game");
+		
+		setLayout(new BorderLayout());
+		myPanel = new MyPanel();
+		add(myPanel, BorderLayout.CENTER);
+		
+		setSize(640, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		new BallGame();
+	}
+
+}
