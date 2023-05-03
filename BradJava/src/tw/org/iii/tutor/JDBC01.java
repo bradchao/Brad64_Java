6b7e891f9e1473e875142e26c0260cf1c99c73de
USER <USER@10.0.104.190>
2023-05-03 15:08:25 +0800

    05031508

--- BradJava/src/tw/org/iii/tutor/JDBC01.java
+++ BradJava/src/tw/org/iii/tutor/JDBC01.java
@@ -2,6 +2,7 @@ package tw.org.iii.tutor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class JDBC01 {
 	public static void main(String[] args) {
@@ -16,6 +17,21 @@ public class JDBC01 {
 		try {
 			Connection conn = DriverManager.getConnection(
 					"jdbc:mysql://localhost:3306/iii?user=root&password=root");
+			
+			Statement stmt = conn.createStatement();
+			String sql = "INSERT INTO cust (cname,tel,birthday) VALUES ('aa','123','1999-01-02')";
+			if (stmt.execute(sql)) {
+				System.out.println("Query");
+			}else {
+				System.out.println("Update");
+			}
+			
+			
+			int count = stmt.executeUpdate(sql);
+			System.out.println(count);
+			
+			
+			
 			System.out.println("OK2");
 		}catch(Exception e) {
 			System.out.println(e);
