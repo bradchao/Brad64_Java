5fcb38af90906984715f4f6ffac78a5d0f6f0168
USER <USER@10.0.104.190>
2023-05-05 09:47:38 +0800

    05050947

--- BradJava/src/tw/org/iii/tutor/JDBC09.java
+++ BradJava/src/tw/org/iii/tutor/JDBC09.java
@@ -2,14 +2,22 @@ package tw.org.iii.tutor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Properties;
 import java.util.Scanner;
+
+import tw.org.iii.myclasses.BCrypt;
 
 public class JDBC09 {
 	private static Connection conn;
 	private static final String USER = "root";
 	private static final String PASSWORD = "root";
 	private static final String URL = "jdbc:mysql://localhost/iii";
+	private static final String SQL_CHECK = 
+		"SELECT count(*) count FROM member WHERE account = ?";
+	private static final String SQL_INSERT = 
+		"INSERT INTO member (account,passwd,email) VALUES (?,?,?)";
 	
 	public static void main(String[] args) {
 
@@ -28,14 +36,49 @@ public class JDBC09 {
 				
 		try {
 			conn = DriverManager.getConnection(URL, prop);
+			if (checkAccount(account)) {
+				if (addMember(account, passwd, email)) {
+					// OK
+					System.out.println("Register success");
+				}else {
+					// XX2
+					System.out.println("Register failure");
+				}
+			}else {
+				// xx1
+				System.out.println("Account exist");
+			}
 			
 		}catch(Exception e) {
 			System.out.println(e);
 		}
 		
+	}
+	
+	static boolean checkAccount(String account) throws Exception {
+		boolean ret;
 		
+		PreparedStatement pstmt = conn.prepareStatement(SQL_CHECK);
+		pstmt.setString(1, account);
+		ResultSet rs = pstmt.executeQuery();
+		rs.next();
+		ret = rs.getInt("count") == 0;
+		rs.close();
+		//rs = null;
 		
+		return ret;
+	}
+	
+	static boolean addMember(String account, String passwd, String email) 
+			throws Exception {
+		boolean isOK;
+		PreparedStatement pstmt = conn.prepareStatement(SQL_INSERT);
+		pstmt.setString(1, account);
+		pstmt.setString(2, BCrypt.hashpw(passwd, BCrypt.gensalt()));
+		pstmt.setString(3, email);
+		isOK = pstmt.executeUpdate() != 0;
 		
+		return isOK;
 	}
 
 }
