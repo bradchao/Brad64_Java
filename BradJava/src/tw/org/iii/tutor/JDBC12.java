fdb753159a09af33758468a70709122826912917
USER <USER@10.0.104.190>
2023-05-05 11:19:45 +0800

    05051119

--- BradJava/src/tw/org/iii/tutor/JDBC12.java
+++ BradJava/src/tw/org/iii/tutor/JDBC12.java
@@ -1,5 +1,11 @@
 package tw.org.iii.tutor;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Properties;
+
 public class JDBC12 {
 	private static final String USER = "root";
 	private static final String PASSWORD = "root";
@@ -14,8 +20,23 @@ public class JDBC12 {
 			+ "GROUP by e.EmployeeID "
 			+ "ORDER by sum desc";
 		
+		Properties prop = new Properties();
+		prop.put("user", USER); prop.put("password", PASSWORD);
+		try {
+			Connection conn = DriverManager.getConnection(URL, prop);
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				System.out.println(rs.getString("Firstname") + ":" + rs.getString("sum"));
+			}
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 		
 		
+		
+
 		
 		
 	}
